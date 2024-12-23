5a4f255ac9942cec37771f25bdb25bd25c774f25
v-cshuai <v-cshuai.gd@chinatelecom.cn>
2024-12-23 18:07:21 +0800

    echarts-map-data

---
+++ data/provinceMap.ts
@@ -0,0 +1,291 @@
+export const provinceMap = new Map([
+  // 总部
+  [
+    100000,
+    {
+      companyCode: 171000,
+      companyName: '天翼电信终端有限公司',
+      provinceId: 0,
+      provinceName: '总部',
+    },
+  ],
+  [
+    410000,
+    {
+      companyCode: 174101,
+      companyName: '天翼终端河南分公司',
+      provinceId: 841,
+      provinceName: '河南省',
+    },
+  ],
+  [
+    110000,
+    {
+      companyCode: 171100,
+      companyName: '天翼终端北京分公司',
+      provinceId: 811,
+      provinceName: '北京市',
+    },
+  ],
+  [
+    420000,
+    {
+      companyCode: 174201,
+      companyName: '天翼终端湖北分公司',
+      provinceId: 842,
+      provinceName: '湖北省',
+    },
+  ],
+  [
+    120000,
+    {
+      companyCode: 171200,
+      companyName: '天翼终端天津分公司',
+      provinceId: 812,
+      provinceName: '天津市',
+    },
+  ],
+  [
+    430000,
+    {
+      companyCode: 174301,
+      companyName: '天翼终端湖南分公司',
+      provinceId: 843,
+      provinceName: '湖南省',
+    },
+  ],
+  [
+    130000,
+    {
+      companyCode: 171301,
+      companyName: '天翼终端河北分公司',
+      provinceId: 813,
+      provinceName: '河北省',
+    },
+  ],
+  [
+    440000,
+    {
+      companyCode: 174401,
+      companyName: '天翼终端广东分公司',
+      provinceId: 844,
+      provinceName: '广东省',
+    },
+  ],
+  [
+    140000,
+    {
+      companyCode: 171401,
+      companyName: '天翼终端山西分公司',
+      provinceId: 814,
+      provinceName: '山西省',
+    },
+  ],
+  [
+    450000,
+    {
+      companyCode: 174501,
+      companyName: '天翼终端广西分公司',
+      provinceId: 845,
+      provinceName: '广西壮族自治区',
+    },
+  ],
+  [
+    150000,
+    {
+      companyCode: 171501,
+      companyName: '天翼终端内蒙古分公司',
+      provinceId: 815,
+      provinceName: '内蒙古自治区',
+    },
+  ],
+  [
+    460000,
+    {
+      companyCode: 174601,
+      companyName: '天翼终端海南分公司',
+      provinceId: 846,
+      provinceName: '海南省',
+    },
+  ],
+  [
+    210000,
+    {
+      companyCode: 172101,
+      companyName: '天翼终端辽宁分公司',
+      provinceId: 821,
+      provinceName: '辽宁省',
+    },
+  ],
+  [
+    500000,
+    {
+      companyCode: 175001,
+      companyName: '天翼终端重庆分公司',
+      provinceId: 850,
+      provinceName: '重庆市',
+    },
+  ],
+  [
+    220000,
+    {
+      companyCode: 172201,
+      companyName: '天翼终端吉林分公司',
+      provinceId: 822,
+      provinceName: '吉林省',
+    },
+  ],
+  [
+    510000,
+    {
+      companyCode: 175101,
+      companyName: '天翼终端四川分公司',
+      provinceId: 851,
+      provinceName: '四川省',
+    },
+  ],
+  [
+    230000,
+    {
+      companyCode: 172301,
+      companyName: '天翼终端黑龙江分公司',
+      provinceId: 823,
+      provinceName: '黑龙江省',
+    },
+  ],
+  [
+    520000,
+    {
+      companyCode: 175201,
+      companyName: '天翼终端贵州分公司',
+      provinceId: 852,
+      provinceName: '贵州省',
+    },
+  ],
+  [
+    310000,
+    {
+      companyCode: 173100,
+      companyName: '天翼终端上海分公司',
+      provinceId: 831,
+      provinceName: '上海市',
+    },
+  ],
+  [
+    530000,
+    {
+      companyCode: 175301,
+      companyName: '天翼终端云南分公司',
+      provinceId: 853,
+      provinceName: '云南省',
+    },
+  ],
+  [
+    320000,
+    {
+      companyCode: 173201,
+      companyName: '天翼终端江苏分公司',
+      provinceId: 832,
+      provinceName: '江苏省',
+    },
+  ],
+  [
+    540000,
+    {
+      companyCode: 175401,
+      companyName: '天翼终端西藏分公司',
+      provinceId: 854,
+      provinceName: '西藏自治区',
+    },
+  ],
+  [
+    330000,
+    {
+      companyCode: 173301,
+      companyName: '天翼终端浙江分公司',
+      provinceId: 833,
+      provinceName: '浙江省',
+    },
+  ],
+  [
+    610000,
+    {
+      companyCode: 176101,
+      companyName: '天翼终端陕西分公司',
+      provinceId: 861,
+      provinceName: '陕西省',
+    },
+  ],
+  [
+    340000,
+    {
+      companyCode: 173401,
+      companyName: '天翼终端安徽分公司',
+      provinceId: 834,
+      provinceName: '安徽省',
+    },
+  ],
+  [
+    620000,
+    {
+      companyCode: 176201,
+      companyName: '天翼终端甘肃分公司',
+      provinceId: 862,
+      provinceName: '甘肃省',
+    },
+  ],
+  [
+    350000,
+    {
+      companyCode: 173501,
+      companyName: '天翼终端福建分公司',
+      provinceId: 835,
+      provinceName: '福建省',
+    },
+  ],
+  [
+    630000,
+    {
+      companyCode: 176301,
+      companyName: '天翼终端青海分公司',
+      provinceId: 863,
+      provinceName: '青海省',
+    },
+  ],
+  [
+    360000,
+    {
+      companyCode: 173601,
+      companyName: '天翼终端江西分公司',
+      provinceId: 836,
+      provinceName: '江西省',
+    },
+  ],
+  [
+    640000,
+    {
+      companyCode: 176401,
+      companyName: '天翼终端宁夏分公司',
+      provinceId: 864,
+      provinceName: '宁夏回族自治区',
+    },
+  ],
+  [
+    370000,
+    {
+      companyCode: 173701,
+      companyName: '天翼终端山东分公司',
+      provinceId: 837,
+      provinceName: '山东省',
+    },
+  ],
+  [
+    650000,
+    {
+      companyCode: 176501,
+      companyName: '天翼终端新疆分公司',
+      provinceId: 865,
+      provinceName: '新疆维吾尔自治区',
+    },
+  ],
+]);
